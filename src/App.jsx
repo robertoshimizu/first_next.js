01edb43e72b3d83354bafdaf020f2bfd6658af89
Roberto Shimizu <rshimizubr@gmail.com>
2023-10-24 20:15:01 -0300

    add useState Hook

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,24 @@
 import { createRoot } from "react-dom/client";
 import Pet from "./Pet";
 import SearchParams from "./SearchParams";
+import Animal from "./Animal";
+import Profile from "./User";
 
 // delete the Pet component
 
 const App = () => {
   return (
     <div>
+      <Profile />
       <h1>Adopt Me!</h1>
       <Pet name="Luna" animal="dog" breed="Havanese" />
       <Pet name="Pepper" animal="bird" breed="Cockatiel" />
       <Pet name="Doink" animal="cat" breed="Mix" />
       <div>
         <SearchParams />
+      </div>
+      <div>
+        <Animal />
       </div>
     </div>
   );
