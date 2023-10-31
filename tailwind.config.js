f7682ce078525f6a63f14a70818d56db07f54544
Roberto Shimizu <rshimizubr@gmail.com>
2023-10-31 10:37:56 -0300

    convert to next.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
+const formsPlugin = require('@tailwindcss/forms')
+const headlessuiPlugin = require('@headlessui/tailwindcss')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {},
+  plugins: [formsPlugin, headlessuiPlugin],
 }
-
